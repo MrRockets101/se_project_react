060cb97863386fee62ece76c6b22de07bab546f2
alex brosovich <alexbrosovich@gmail.com>
2025-09-24 15:02:53 -0400

    spelling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer__container">
-        <p className="footer__text">Devolpoed by Alexander Brosovich</p>
+        <p className="footer__text">Developed by Alexander Brosovich</p>
         <p className="footer__text">2025</p>
       </div>
     </footer>
